3773089b30d5a89dc60a521a994e24e04b32405a
Nedoxy <nk.nedum@gmail.com>
2022-03-30 03:13:04 +0100

    Added KogaWebTest,Project Object Model(POM),and Test automation files

---
+++ testAutomation.java
@@ -0,0 +1,21 @@
+public class testAutomation {
+    //You want to test a signup flow for selenium blog
+
+    /***
+     * 1. Open your Chrome browser
+     * 2. Open your selected app URL(https://konga.com)
+     * 3. Maximize the window
+     * 4. Get the page title
+     * 5. Click on the login button
+     * 6. Input an email address on the email field
+     * 7. Input your password on the password field
+     * 8. Click on the login button
+     * 9. Click on the username
+     * 10.Click on logout
+     * 11.Quit the browser
+
+     
+     */
+
+
+}
